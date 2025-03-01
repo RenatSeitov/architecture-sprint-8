610a3516f1d992c000a6b27e3793164dff2a7b89
root <root@SLB-61XX3D3.SLB.RU>
2025-03-01 09:10:28 +0300

    upd

--- api/config.py
+++ api/config.py
@@ -1,8 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from keycloak import KeycloakOpenID
-from keycloak import KeycloakAdmin
-
 
 
 class Settings(BaseSettings):
@@ -10,8 +8,6 @@ class Settings(BaseSettings):
     keycloak_realm: str = Field(..., env="KEYCLOAK_REALM")
     keycloak_client_id: str = Field(..., env="KEYCLOAK_CLIENT_ID")
     keycloak_client_secret: str = Field(..., env="KEYCLOAK_CLIENT_SECRET")
-    keycloak_admin_username = str = Field(..., env="KEYCLOAK_ADMIN_USERNAME")
-    keycloak_admin_password = str = Field(..., env="KEYCLOAK_ADMIN_PASSWORD")
 
     class Config:
         env_file = ".env"
@@ -25,13 +21,6 @@ keycloak_openid = KeycloakOpenID(
     realm_name=settings.keycloak_realm,
     client_id=settings.keycloak_client_id,
     client_secret_key=settings.keycloak_client_secret,
-)
-
-keycloak_admin = KeycloakAdmin(
-    server_url=settings.keycloak_server_url,
-    realm_name=settings.keycloak_realm,
-    username=settings.keycloak_admin_username,
-    password=settings.keycloak_admin_password,
 )
 
 
--- api/service.py
+++ api/service.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, status
 from keycloak.exceptions import KeycloakAuthenticationError
-from api.config import keycloak_openid, keycloak_admin
+from api.config import keycloak_openid
 from api.models import Report
 
 REQUIRED_ROLES=[
@@ -16,20 +16,11 @@ class AuthService:
         Optionally, check if user has required roles.
         """
         try:
-            user_info = keycloak_openid.userinfo(token)
+            user_info = keycloak_openid.introspect(f"Bearer {token}")
+            print(user_info)
             if not user_info:
                 raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
-                )
-
-            user_id = keycloak_admin.get_user_id(username=user_info["preferred_username"])
-
-            user_roles = keycloak_admin.get_user_roles(user_id)
-
-            if not any(role in REQUIRED_ROLES for role in user_roles):
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="User does not have required roles"
                 )
 
             return Report(
@@ -37,6 +28,7 @@ class AuthService:
                 email=user_info.get("email"),
                 full_name=user_info.get("name"),
             )
+            return user_info
         except KeycloakAuthenticationError as exc:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
